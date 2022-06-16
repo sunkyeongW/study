8667c5a322b87c1d434b0647de3e7bd928f1f4b5
sunkyeongW <bluelight0949@naver.com>
2022-06-17 02:48:11 +0900

    파이썬의 데이터 타입 학습과 실습 파일 생성.

---
+++ part4/python_4.py
@@ -0,0 +1,28 @@
+print("1.number type")
+print("4+1",4+1)
+print("234234234-23424",234234234-23424)
+print("3453*34",3453*34)
+print("2424/24",2424/24)
+
+import math
+print(math.sqrt(4))
+print(math.pow(4,4))
+
+print("2.text type")
+print('hi')
+print('''
+hi
+hello
+''')
+print("change word->text",'word replace'.replace('word','text'))
+
+print("3.list type")
+name = ["dkung","growing","skung","star","goodboy"]
+age = [30,34,26,110,17]
+print(name[1])
+print(len(name))
+print('young age',min(age))
+print('old age',max(age))
+
+import statistics
+print('mean',statistics.mean(age))
---
+++ part4/readme4.md
@@ -0,0 +1,38 @@
+파이썬 데이터 타입
+1.데이터 타입
+2.텍스트 타입
+3.리스트 타입
+
+1.데이터 타입
+init(정수)
+float(실수)
+'+' 덧셈
+'-' 뺄셈
+'*' 곱셈
+'/' 나눗셈
+
+모듈: math, random
+math.sqrt-제곱근
+math.pow-제곱
+
+2.텍스트 타입
+string(문자열)
+'  ' or "  " 
+문자열+ 문자열
+:여기서 +는 결합연산이다.
+
+모듈 : len(문자의 길이), replace(문자 바꾸기)
+
+3.리스트 타입
+통계를 낼 때 유용하다.
+변수 = "[  ,   ,   ]"
+
+데이터 가져오기
+student = "["one","two","three"]
+print ("student[1]" , student[1])
+=student[1],two 라는 결과가 나옴(여기서 순서는 0부터 따짐)
+
+len(student): 갯수는 1부터 시작
+
+모듈 : len , min, max , statistics(통계), random
+statistic mean(평균값)
