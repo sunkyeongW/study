4a34af6c3890099aa2090a4c601f539b1598bead
sunkyeongW <bluelight0949@naver.com>
2022-06-18 00:14:35 +0900

    readme_inf.md->read_inf1.md 파일네임 수정. readme_inf2.md 파일 생성. python_inf2.py 학습을 통해 코드 생성.

---
+++ inf/inf_part1/python_inf2.py
@@ -0,0 +1,46 @@
+#기본선언
+num = 3000
+print()
+#출력
+print(num)
+print(type(num))
+print()
+#동시 선언
+a = b = c = 3000
+print(a,b,c)
+print()
+#재선언
+star = 17
+star = "cutie"
+print()
+print(star)
+print(type(star))
+print()
+#object references
+print()
+#id(identify)
+m = 300
+n = 89
+print()
+print(id(m))
+print(id(n))
+print()
+m = 300
+n = 300
+print()
+print(id(m))
+print(id(n))
+print(id(m) == id(n))
+print()
+#다양한 변수 선언
+age = 1
+Age = 2
+aGe = 3
+AGE = 4
+a_g_e = 5
+_age = 6
+age_ = 7
+_AGE_ = 8
+print()
+#예약어는 변수명으로 불가능
+'False' 'raise'
